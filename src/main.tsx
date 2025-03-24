be3b2b966548077f4f2945e1837b27e3289aa307
Arjav5090 <arjavvirani123@gmail.com>
2025-03-23 23:29:17 -0400

    Gh pages config

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,12 @@ import "./index.css";
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
-import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <AppWrapper>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
-        </BrowserRouter>
-      
       </AppWrapper>
     </ThemeProvider>
   </StrictMode>
